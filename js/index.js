def9f2192afe6b57e8ce86ab2f99e3f3cebb71ea
buaagyx2012 <buaagyx2012@gmail.com>
2016-09-12 23:23:42 +0800

    add makisu menu

--- js/index.js
+++ js/index.js
@@ -1,3 +1,44 @@
 /**
  * Created by yzz on 2016/9/12.
  */
+
+
+$(function(){
+
+    function init() {
+        if ($.fn.makisu.enabled) {
+
+            var $menu = $('.list');
+
+            // Create Makisus
+
+            $menu.makisu({
+                selector: 'dd',
+                overlap: 0.85,
+                speed: 1.7
+            });
+
+
+            // Open all
+
+            $menu.makisu('open');
+
+            // Toggle on click
+
+            $('.menu-toggle').on('click', function () {
+                $(this).parent().makisu("toggle");
+                //$('.list').makisu('toggle');
+            });
+
+            // Disable all links
+
+
+        } else {
+
+            $('.warning').show();
+        }
+    }
+
+    init();
+
+});
